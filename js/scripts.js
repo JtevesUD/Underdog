8f36f96537e143bb70d8ae7a041220aa0d2db2b0
Jonathan Teves <j.teves07@gmail.com>
2021-01-05 11:18:11 -0500

    Updated homepage video for macbook size + Mobile.
    Added custom cotrols to video.

--- js/scripts.js
+++ js/scripts.js
@@ -20,15 +20,29 @@ window.onclick = (event) => {
 
 var video = document.querySelector('.video');
 var juice = document.querySelector('.orange-juice');
-var btn = document.queryElementById('play-pause');
+var btn = document.getElementById('play-pause');
+
+
 
 function togglePlayPause(){
     if(video.paused){
         btn.className= "pause";
         video.play();
-    }
-    else{
+    }else{
         btn.className = "play";
         video.pause();
     }
 }
+
+
+btn.onclick = function() {
+    togglePlayPause();
+};
+
+video.addEventListener('timeupdate', function(){
+    var juicePos = video.currentTime / video.duration;
+    juice.style.width = juicePos * 100 + "%";
+    if(video.ended){
+        btn.className="play";
+    }
+});
